ccd38ea6501949e38a86385661ddd938c8ae9eef
Bewnet Addisalem <bewnetaddisalem@gmail.com>
2026-05-09 06:19:11 +0900

    New user role have been added

--- api/models.py
+++ api/models.py
@@ -10,6 +10,7 @@ class User(AbstractUser):
         ('admin', 'Admin'),
         ('agent', 'Agent'),
         ('accountant', 'Accountant'),
+        ('marketing', 'Marketing'),
     )
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='agent')
     name = models.CharField(max_length=255)
